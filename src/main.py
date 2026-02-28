3f306ed08e890d8f107ec94861cb04d61cce4a57
Ananyo Bhattacharya <ananyobhattacharya10@gmail.com>
2026-02-28 11:43:39 +0530

    Add init method

--- src/main.py
+++ src/main.py
@@ -1,6 +1,15 @@
-def main() -> None:
-    print("Hello from $projname!")
+import os
 
 
-if __name__ == "__main__":
-    main()
+def init(path=".") -> None:
+    base = os.path.join(path, ".tinygit")
+    objects = os.path.join(base, "objects")
+    refs_heads = os.path.join(base, "refs", "heads")
+    paths = [base, objects, refs_heads]
+    for p in paths:
+        os.makedirs(p, exist_ok=True)
+
+    with open(os.path.join(base, "HEAD"), "w") as head:
+        head.write("ref: refs/heads/main\n")
+
+    return
