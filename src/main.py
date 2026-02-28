585430fd34f83fb9c0225dd898a270bd76e4cf32
Ananyo Bhattacharya <ananyobhattacharya10@gmail.com>
2026-02-28 11:43:55 +0530

    Add write object method

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
+import hashlib
 import os
+import zlib
 
 
 def init(path=".") -> None:
@@ -13,3 +15,24 @@ def init(path=".") -> None:
         head.write("ref: refs/heads/main\n")
 
     return
+
+
+def write_object(data: bytes, tinygit_dir=".tinygit") -> str:
+    store_content = b"blob "
+    store_content += str(len(data)).encode()
+    store_content += b"\0"
+    store_content += data
+
+    sha1hash = hashlib.sha1(store_content).hexdigest()
+
+    dirname, filename = sha1hash[:2], sha1hash[2:]
+
+    compressed = zlib.compress(store_content)
+
+    dirpath = os.path.join(tinygit_dir, "objects", dirname)
+    os.makedirs(dirpath, exist_ok=True)
+
+    with open(os.path.join(dirpath, filename), "wb") as file:
+        file.write(compressed)
+
+    return sha1hash
